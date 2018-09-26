2e039759c98698d996f330d451371233e7ad4854
Aditi Garg <aditi.garg@jungleworks.com>
2018-09-26 15:23:11 +0530

    Initial Commit

---
+++ ApplePay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePay
+//
+//  Created by cl-macmini-128 on 26/09/18.
+//  Copyright © 2018 cl-macmini-128. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
